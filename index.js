d2faba3b22fb8abe9176d2b3551c7f662008882d
unknown <nishavkumar07@gmail.com>
2025-06-27 18:09:01 +0530

    added fetch:

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const OpenAI = require('openai');
 const app = express();
 const port = 3000;
 const path = require("path");
-
+app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname , "views"));
 app.use(express.static(path.join(__dirname , "public")));
+
+const chatData = [];
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
